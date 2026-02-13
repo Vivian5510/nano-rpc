9934ab66545f3f7eedf35980ff5979a3deb71f75
Rosy <2156722358@qq.com>
2026-02-14 00:26:01 +0800

    feat(rpc/processor): add heartbeat processor

--- nano-protocol-rpc/src/main/java/com/rosy/nano/protocol/rpc/processor/RpcHeartbeatProcessor.java
+++ nano-protocol-rpc/src/main/java/com/rosy/nano/protocol/rpc/processor/RpcHeartbeatProcessor.java
@@ -1,12 +1,21 @@
 package com.rosy.nano.protocol.rpc.processor;
 
+import com.rosy.nano.protocol.rpc.command.RpcResponseCode;
+import com.rosy.nano.protocol.rpc.command.header.HeartbeatResponseHeader;
 import com.rosy.nano.transport.command.RemotingCommand;
 import com.rosy.nano.transport.processor.RequestProcessor;
 import io.netty.channel.ChannelHandlerContext;
+import com.rosy.nano.transport.serialization.HeaderSerializeType;
 
 public class RpcHeartbeatProcessor implements RequestProcessor {
     @Override
     public RemotingCommand process(ChannelHandlerContext ctx, RemotingCommand request) {
-        return null;
+        if (request.isOneWay()) {
+            throw new IllegalArgumentException("RPC_HEARTBEAT doesn't accept oneway request");
+        }
+
+        HeaderSerializeType hType = request.getHeaderSerializeType();
+        HeartbeatResponseHeader header = new HeartbeatResponseHeader(System.currentTimeMillis());
+        return RemotingCommand.newResponse(RpcResponseCode.SUCCESS, request, header, hType);
     }
 }
